47cf5d1cd97a628fa2fb2775b4e4f5f228baccc4
kat3005 <kasia.alicja@gmail.com>
2019-07-31 13:36:01 +0100

    Adds first completed test

--- models/cinema.js
+++ models/cinema.js
@@ -2,4 +2,15 @@ const Cinema = function (films) {
   this.films = films;
 };
 
+
+Cinema.prototype.titles = function (films) {
+  let result = this.films.filter((film) => {
+    return film.title;
+  })
+  return result;
+};
+
+
+
+
 module.exports = Cinema;
